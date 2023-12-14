628c74d8a7109f186632380a6262ec34a55a8fe4
mooud <mooudgamer@gmail.com>
2023-12-14 18:10:03 +0330

    chat page updated (markdown and simple chat added for test)

--- src/app/chat/[roomId]/page.tsx
+++ src/app/chat/[roomId]/page.tsx
@@ -1,21 +1,49 @@
-'use client';
+"use client";
+import { MarkDownDefault } from "@/app/utils/MarkDown";
 import NavLink from "@/components/ui/NavLink";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
+import Markdown, { Components } from "react-markdown";
+import rehypeRaw from "rehype-raw";
+import remarkGfm from "remark-gfm";
 
 export default function Chat() {
+  const [textAreaHeight, setTextAreaHeight] = useState(52);
+  const [messages, setMessages] = useState([
+    {
+      bot: false,
+      message: `
+Hi,you can help me to write javascrip code?
+    `,
+    },
+    {
+      bot: true,
+      message: `
+Hello, I am FleetFox.
+> Yes I can help you!
+> For example i can generate code like this : 
+\`\`\`js
+for let i in Array(5){
+  console.log('Hello FleetFox!');
+}
+\`\`\`
+      `,
+    },
+  ]);
 
-  const [textAreaHeight, setTextAreaHeight] = useState(52)
-
-  const inputKeyDownHandler = (e:React.KeyboardEvent<HTMLTextAreaElement>)=>{
-    if(e.shiftKey && e.key === 'Enter'){
-      setTextAreaHeight(textAreaHeight + 24)
-    }    
-    if(textAreaHeight > 52 && e.key === "Backspace" && e.currentTarget.value.replace(/\s/g, '') === ''){
-      setTextAreaHeight(textAreaHeight - 24)
+  const inputKeyDownHandler = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.shiftKey && e.key === "Enter") {
+      setTextAreaHeight(textAreaHeight + 24);
     }
-  }
+    if (
+      textAreaHeight > 52 &&
+      e.key === "Backspace" &&
+      e.currentTarget.value.replace(/\s/g, "") === ""
+    ) {
+      setTextAreaHeight(textAreaHeight - 24);
+    }
+  };
 
   return (
     <div className="relative flex h-full max-w-full flex-1 flex-col overflow-hidden">
@@ -23,67 +51,127 @@ export default function Chat() {
         <div role="presentation" className="flex h-full flex-col">
           <div className="flex-1 overflow-hidden">
             <div className="relative h-full">
-              <div className="absolute left-0 right-0">
-                <div className="sticky top-0 mb-1.5 flex items-center justify-between z-10 h-14 bg-white/95 p-2 font-semibold dark:bg-zinc-800/90">
-                  <div className="flex items-center gap-2">
-                    <div
-                      className="group flex cursor-pointer items-center gap-1 rounded-xl py-2 px-3 text-lg font-medium hover:bg-zinc-50 radix-state-open:bg-zinc-50 dark:hover:bg-black/10 dark:radix-state-open:bg-black/20"
-                      id="radix-:r1l:"
-                      aria-haspopup="menu"
-                      aria-expanded="false"
-                      data-state="closed">
-                      <div>
-                        v{" "}
-                        <span className="text-token-text-secondary">0.1</span>
+              {messages ? (
+                <>
+                  <div className="sticky top-0 mb-1.5 flex items-center justify-between z-10 h-14 bg-white/95 p-2 font-semibold dark:bg-zinc-800/90">
+                    <div className="flex items-center gap-2">
+                      <div
+                        className="group flex cursor-pointer items-center gap-1 rounded-xl py-2 px-3 text-lg font-medium hover:bg-zinc-50 radix-state-open:bg-zinc-50 dark:hover:bg-black/10 dark:radix-state-open:bg-black/20"
+                        id="radix-:r1l:"
+                        aria-haspopup="menu"
+                        aria-expanded="false"
+                        data-state="closed">
+                        <div>
+                          v{" "}
+                          <span className="text-token-text-secondary">0.1</span>
+                        </div>
+                        <svg
+                          width="16"
+                          height="17"
+                          viewBox="0 0 16 17"
+                          fill="none"
+                          className="text-token-text-tertiary">
+                          <path
+                            d="M11.3346 7.83203L8.00131 11.1654L4.66797 7.83203"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"></path>
+                        </svg>
                       </div>
-                      <svg
-                        width="16"
-                        height="17"
-                        viewBox="0 0 16 17"
-                        fill="none"
-                        className="text-token-text-tertiary">
-                        <path
-                          d="M11.3346 7.83203L8.00131 11.1654L4.66797 7.83203"
-                          stroke="currentColor"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"></path>
-                      </svg>
+                    </div>
+                    <div className="flex gap-2 pr-1"></div>
+                  </div>
+                  <div className="flex flex-col gap-5">
+                    {messages.map((m, i) =>
+                      m.bot ? (
+                        <div
+                          key={i}
+                          className="flex flex-1 text-base gap-2 px-5 lg:px-10 xl:px-10 md:max-w-3xl lg:max-w-[40rem] xl:max-w-[48rem] group">
+                          <div>
+                            <Image
+                              src={"/images/logo.svg"}
+                              width={30}
+                              height={30}
+                              alt="avatar"
+                              className="rounded-full"
+                            />
+                          </div>
+                          <div className="bg-border-gradient border border-transparent p-2 rounded-lg rounded-tl-none shadow-md">
+                            <div className="mb-1 font-bold">FleetFox</div>
+                            <Markdown
+                              remarkPlugins={[remarkGfm]}
+                              rehypePlugins={[rehypeRaw]}
+                              className="h-full overflow-y-auto break-words"
+                              components={
+                                MarkDownDefault as Partial<Components>
+                              }>
+                              {m.message}
+                            </Markdown>
+                          </div>
+                        </div>
+                      ) : (
+                        <div
+                          key={i}
+                          className="flex flex-1 text-base gap-2 px-5 lg:px-10 xl:px-10 md:max-w-3xl lg:max-w-[40rem] xl:max-w-[48rem] group">
+                          <div>
+                            <Image
+                              src={"/images/avatar.jpg"}
+                              width={30}
+                              height={30}
+                              alt="avatar"
+                              className="rounded-full"
+                            />
+                          </div>
+                          <div className="bg-zinc-700 p-2 rounded-lg rounded-tl-none shadow-md">
+                            <div className="mb-1 font-bold">You</div>
+                            <Markdown
+                              remarkPlugins={[remarkGfm]}
+                              rehypePlugins={[rehypeRaw]}
+                              className="h-full overflow-y-auto break-words"
+                              components={
+                                MarkDownDefault as Partial<Components>
+                              }>
+                              {m.message}
+                            </Markdown>
+                          </div>
+                        </div>
+                      )
+                    )}
+                  </div>
+                </>
+              ) : (
+                <div className="flex h-full flex-col items-center justify-center">
+                  <div className="relative">
+                    <div className="mb-3 h-[72px] w-[72px]">
+                      <div className="gizmo-shadow-stroke relative flex h-full items-center justify-center rounded-full bg-white text-black">
+                        <img src="/images/logo.svg" />
+                      </div>
                     </div>
                   </div>
-                  <div className="flex gap-2 pr-1"></div>
-                </div>
-              </div>
-              <div className="flex h-full flex-col items-center justify-center">
-                <div className="relative">
-                  <div className="mb-3 h-[72px] w-[72px]">
-                    <div className="gizmo-shadow-stroke relative flex h-full items-center justify-center rounded-full bg-white text-black">
-                      <img src="/images/logo.svg"/>
-                    </div>
+                  <div className="mb-5 text-2xl font-medium">
+                    How can I help you today?
                   </div>
                 </div>
-                <div className="mb-5 text-2xl font-medium">
-                  How can I help you today?
-                </div>
-              </div>
+              )}
             </div>
           </div>
           <div className="w-full pt-2 md:pt-0 dark:border-white/20 md:border-transparent md:dark:border-transparent md:w-[calc(100%-.5rem)]">
             <form className="stretch mx-2 flex flex-row gap-3 last:mb-2 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
               <div className="relative flex h-full flex-1 items-stretch md:flex-col">
                 <div className="flex w-full items-center">
-                  <div className="overflow-hidden [&amp;:has(textarea:focus)]:border-token-border-xheavy [&amp;:has(textarea:focus)]:shadow-[0_2px_6px_rgba(0,0,0,.05)] flex flex-col w-full dark:border-token-border-heavy flex-grow relative border border-token-border-heavy dark:text-white rounded-2xl bg-white dark:bg-zinc-800 shadow-[0_0_0_2px_rgba(255,255,255,0.95)] dark:shadow-[0_0_0_2px_rgba(52,53,65,0.95)]">
+                  <div className="overflow-hidden [&:has(textarea:focus)]:border-token-border-xheavy [&:has(textarea:focus)]:shadow-[0_2px_6px_rgba(0,0,0,.05)] flex flex-col w-full dark:border-token-border-heavy flex-grow relative border border-token-border-heavy dark:text-white rounded-2xl bg-white dark:bg-zinc-800 shadow-[0_0_0_2px_rgba(255,255,255,0.95)] dark:shadow-[0_0_0_2px_rgba(52,53,65,0.95)]">
                     <textarea
                       id="prompt-textarea"
                       tabIndex={0}
                       data-id="root"
                       rows={1}
                       onKeyDown={inputKeyDownHandler}
-                      style={{height:textAreaHeight,maxHeight:200}}
+                      style={{ height: textAreaHeight, maxHeight: 200 }}
                       placeholder="Message FleetFox…"
                       className="m-0 w-full resize-none border-0 bg-transparent py-[10px] pr-10 focus:ring-0 focus-visible:ring-0 dark:bg-transparent md:py-3.5 md:pr-12 placeholder-black/50 dark:placeholder-white/50 pl-3 md:pl-4"></textarea>
                     <button
-                      className="absolute md:bottom-3 md:right-3 dark:hover:opacity-70 transition dark:disabled:hover:bg-transparent right-2 dark:disabled:bg-white disabled:bg-black disabled:opacity-10 disabled:text-zinc-400 enabled:bg-gradient-to-br from-yellow-500 to-orange-500 text-white p-0.5 border border-black rounded-lg dark:border-yellow-400 dark:bg-white bottom-1.5 transition-colors"
+                      className="absolute md:bottom-3 md:right-3 dark:hover:opacity-70 transition dark:disabled:hover:bg-transparent right-2 dark:disabled:bg-white disabled:bg-black disabled:opacity-10 disabled:text-zinc-400 enabled:bg-gradient-to-br from-yellow-500 to-orange-500 text-white p-0.5 border border-black rounded-lg dark:border-yellow-400 dark:bg-white bottom-1.5"
                       data-testid="send-button">
                       <span className="" data-state="closed">
                         <svg
@@ -107,8 +195,10 @@ export default function Chat() {
             </form>
             <div className="relative px-2 py-2 text-center text-xs text-zinc-600 dark:text-zinc-300 md:px-[60px]">
               <span>
-                <span className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 to-orange-500">FleetFox</span> can make mistakes. Consider checking important
-                information.
+                <span className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 to-orange-500">
+                  FleetFox
+                </span>{" "}
+                can make mistakes. Consider checking important information.
               </span>
             </div>
           </div>
---
+++ src/app/utils/MarkDown/index.jsx
@@ -0,0 +1,64 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { materialDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+export const MarkDownDefault = {
+  a: ({ node, children, ...props }) => (
+    <a
+      {...props}
+      className="text-blue-500 hover:text-blue-400"
+      style={{ textDecoration: "1px solid blue underline" }}>
+      {children}
+    </a>
+  ),
+  pre: ({ node, children, ...props }) => (
+    <pre {...props} className="rounded-lg overflow-hidden">
+      {children}
+    </pre>
+  ),
+  ol: ({ node, children, ...props }) => (
+    <ol {...props} className="list-decimal px-4 ms-2">
+      {children}
+    </ol>
+  ),
+  ul: ({ node, children, ...props }) => (
+    <ul {...props} className="list-disc px-4 ms-2">
+      {children}
+    </ul>
+  ),
+  h1: ({ node, children, ...props }) => (
+    <h1 {...props} className="text-3xl py-3 mb-3 border-b border-b-slate-500/50">
+      {children}
+    </h1>
+  ),
+  h2: ({ node, children, ...props }) => (
+    <h2 {...props} className="text-2xl py-3 mb-3 border-b border-b-slate-500/50">
+      {children}
+    </h2>
+  ),
+  h3: ({ node, children, ...props }) => (
+    <h3 {...props} className="text-xl py-3 mb-3 border-b border-b-slate-500/50">
+      {children}
+    </h3>
+  ),
+  img: ({ node, children, ...props }) => (
+    <img {...props} className="w-full sm:w-1/2 p-2"/>
+  ),
+  code({ node, inline, className, children, ...props }) {
+    const match = /language-(\w+)/.exec(className || "");
+    return !inline && match ? (
+      <SyntaxHighlighter
+        {...props}
+        style={materialDark}
+        language={match[1]}
+        PreTag="div">
+        {String(children).replace(/\n$/, "")}
+      </SyntaxHighlighter>
+    ) : (
+      <code {...props} className={`${className} bg-slate-500/50 px-1 py-[2px] rounded-md`}>
+        {children}
+      </code>
+    );
+  },
+};
